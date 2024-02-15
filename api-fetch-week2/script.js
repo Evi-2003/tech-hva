eadf906f5b8535a19b2ab1f3f4438b6c51986549
Evi <69012052+Evi-2003@users.noreply.github.com>
2024-02-15 10:36:57 +0100

    Added assignment week 2 lesson 2 api fetching

---
+++ api-fetch-week2/script.js
@@ -0,0 +1,19 @@
+const image = document.querySelector("#duckImage");
+const body = document.querySelector("body");
+async function getDuck() {
+  const ducksFromApi = await fetch(
+    "https://random-d.uk/api/random?format=json"
+  );
+  const duckJson = await ducksFromApi.json();
+  image.src = duckJson.url;
+  return duckJson.url;
+}
+
+async function setDuck() {
+  const duckUrl = await getDuck();
+  console.log(duckUrl);
+  image.src = duckUrl;
+}
+
+getDuck();
+body.addEventListener("keypress", setDuck);
